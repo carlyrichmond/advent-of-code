0926c0018db1e7a0adbbd503509c53cdcd29f67a
Carly Richmond <carlylrichmond@gmail.com>
2020-12-04 10:14:23 +0000

    Day three part 2

--- 2020/day-three.py
+++ 2020/day-three.py
@@ -2,7 +2,7 @@ def generate_matrix():
     tree_map = []
 
     for line in open("2020/input/day-three.txt", "r"):
-        tree_map = tree_map + [list(line[0:-1])]    
+        tree_map = tree_map + [list(line.strip("\n"))]    
     
     return tree_map
 
@@ -10,26 +10,31 @@ def has_tree(tree_map, prior, current):
     return tree_map[current[1]][current[0]] == "#"
 
 def main(): 
-    trees = 0
+    result = 1 # One to ensure the multiplication isn't cancelled out initially
     tree_map = generate_matrix()
-
-    prior_x = 0
-    prior_y = 0
-
-    x = 0
-    y = 0
     
-    while (y < len(tree_map) - 1):
-        prior_x = x
-        prior_y = y
+    for slope in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]:
+        prior_x = 0
+        prior_y = 0
 
-        x = (x + 3) % len(tree_map[y])
-        y += 1
+        x = 0
+        y = 0
 
-        if (has_tree(tree_map, (prior_x, prior_y), (x, y))):
-            trees += 1
+        trees = 0
 
-    print(trees)
+        while (y < len(tree_map) - 1):
+            prior_x = x
+            prior_y = y
+
+            y += slope[1]
+            x = (x + slope[0]) % len(tree_map[y])
+            
+            if (has_tree(tree_map, (prior_x, prior_y), (x, y))):
+                trees += 1
+
+        result *= trees
+
+    print(result)
 
 if __name__ == "__main__":
     main()
