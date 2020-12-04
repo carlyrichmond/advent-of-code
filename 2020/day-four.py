ab5d8b304d262b3f039a5a489ce3cd6c67db1da3
Carly Richmond <carlylrichmond@gmail.com>
2020-12-04 11:56:46 +0000

    Day 4 part 2

--- 2020/day-four.py
+++ 2020/day-four.py
@@ -1,9 +1,11 @@
+import re
+
 class Passport:
 
     def __init__(self, attributes):
-        self.birth_year = attributes["byr"] if "byr" in attributes else ""
-        self.issue_year = attributes["iyr"] if "iyr" in attributes else "" 
-        self.expiration_year = attributes["eyr"] if "eyr" in attributes else ""
+        self.birth_year = int(attributes["byr"]) if "byr" in attributes else None
+        self.issue_year = int(attributes["iyr"]) if "iyr" in attributes else None 
+        self.expiration_year = int(attributes["eyr"]) if "eyr" in attributes else ""
         self.height = attributes["hgt"] if "hgt" in attributes else ""
         self.hair_colour = attributes["hcl"] if "hcl" in attributes else ""
         self.eye_colour = attributes["ecl"] if "ecl" in attributes else ""
@@ -11,19 +13,80 @@ class Passport:
         self.country_id = attributes["cid"] if "cid" in attributes else ""
 
     def is_valid(self):
-        return (self.birth_year and self.issue_year and self.expiration_year and 
-            self.height and self.hair_colour and self.eye_colour and self.passport_id)
+        return (self.is_birth_year_valid() and self.is_issue_year_valid() 
+            and self.is_expiration_year_valid() and self.is_height_valid() 
+            and self.is_hair_colour_valid() and self.is_eye_colour_valid() 
+            and self.is_passport_id_valid())
+
+    # Helper function for diagnosing validation issues
+    def validity_output(self):
+        print(self.passport_id)
+        print("byr: %s", self.is_birth_year_valid())
+        print("iyr: %s", self.is_issue_year_valid())
+        print("eyr: %s", self.is_expiration_year_valid())
+        print("hgt: %s", self.is_height_valid())
+        print("hcl: %s", self.is_hair_colour_valid())
+        print("ecl: %s", self.is_eye_colour_valid())
+        print("pid: %s", self.is_passport_id_valid())
+        
+        return
+
+    # byr (Birth Year) - four digits; at least 1920 and at most 2002.
+    def is_birth_year_valid(self):
+        return self.birth_year in range(1920, 2003)
+    
+    # iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+    def is_issue_year_valid(self):
+        return self.issue_year in range(2010, 2021)
+        
+    # eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+    def is_expiration_year_valid(self):
+        return self.expiration_year in range(2020, 2031)
+
+    # hgt (Height) - a number followed by either cm or in:
+    # If cm, the number must be at least 150 and at most 193.
+    # If in, the number must be at least 59 and at most 76.
+    def is_height_valid(self):
+        height_expression = re.compile("^[0-9]{3}cm$|^[0-9]{2}in$")
+
+        if (not height_expression.match(self.height)):
+            return False
+
+        height_number = int(self.height[0:-2])
+
+        if ("cm" in self.height):
+            return height_number in range(150, 194)
+        
+        else: 
+            return height_number in range(59, 77)
+        
+    # hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+    def is_hair_colour_valid(self):
+        hair_colour_expression = re.compile("^#(?:[a-f0-9]{6})$")
+        return hair_colour_expression.match(self.hair_colour)
+
+    # ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+    def is_eye_colour_valid(self):
+        return self.eye_colour in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+
+    # pid (Passport ID) - a nine-digit number, including leading zeroes.
+    def is_passport_id_valid(self):
+        passport_id_expression = re.compile("^\d{9}$")
+        return passport_id_expression.match(self.passport_id)
 
 def main():
 
     current_attributes = {}
     valid_passport_count = 0
 
-    ## issue with checking the last passport
-    # adding an extra new line to end of file to ensure it is validated
-    open("2020/input/day-four.txt", "a").write("\n")
+    filename = "2020/input/day-four.txt"
 
-    for line in open("2020/input/day-four.txt", "r"):
+    # issue with checking the last passport
+    # adding an extra new line to end of file to ensure it is validated
+    # although this does introduce problems on reruns
+    open(filename, "a").write("\n")
+
+    for line in open(filename, "r"):
         if (line != "\n"):
             elements = line.split(" ")
             
@@ -33,7 +96,6 @@ def main():
             
         else:
             passport = Passport(current_attributes)
-            print("%s: %s" % (passport.passport_id, passport.is_valid()))
 
             if (passport.is_valid()):
                 valid_passport_count += 1
